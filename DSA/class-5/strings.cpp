85897210bd04e244e46e5a30710b6d9d715ec746
rajaX07 <kumar.rajavishvakarma@gmail.com>
2026-03-30 22:16:46 +0530

    solved string ques.

---
+++ DSA/class-5/strings.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    // char ch1, ch2;
+    // cin>> ch1 >> ch2;
+
+    // if(ch1 == ch2){
+    //     cout<< "Yes";
+    // }
+    // else{
+    //     cout<< "No";
+    // }
+
+// Ascii value
+
+    // char ch;
+    // cin>>ch;
+
+    // cout<<(int)ch << endl;
+
+// char 
+    int val;
+    cin>> val;
+    cout<< (char)val<<endl;
+    
+    return 0;
+}
